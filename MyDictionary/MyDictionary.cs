4d9fa50237005b894c76cb7558653052041dfea3
İsmail Candan <ismailcandan44@gmail.com>
2021-02-28 18:55:53 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, Y>
+    {
+        T[] forKey;
+        Y[] forValue;
+
+        public MyDictionary()
+        {
+            forKey = new T[0];
+            forValue= new Y[0];
+        }
+        public void Add(T key,Y value)
+        {
+            T[] tempForKey = forKey;
+            Y[] tempForValue = forValue;
+
+            forKey = new T[forKey.Length + 1];
+            forValue = new Y[forValue.Length + 1];
+
+            for (int i = 0; i < tempForKey.Length; i++)
+            {
+                forKey[i] = tempForKey[i];
+            }
+            for (int i = 0; i < tempForValue.Length; i++)
+            {
+                forValue[i] = tempForValue[i];
+            }
+
+            forKey[forKey.Length - 1] = key;
+            forValue[forValue.Length - 1] = value;
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string,string>();
+            myDictionary.Add("ismail", "candan");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
